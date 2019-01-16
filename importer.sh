548ae468de0415d9ca250a1b81d0b929026776f7
Francesco Esposito <33671357+ovfl0w@users.noreply.github.com>
2019-01-16 15:45:05 +0100

    Update importer.sh

--- importer.sh
+++ importer.sh
@@ -4,7 +4,7 @@
 #------------------------------------------------ 
 # import a Monefy CSV on a MySQL/MariaDB table  		
 #
-#	@f3sposito (Francesco Esposito)  
+#	@ovfl0w (Francesco Esposito)  
 #################################################
 #path
 PATH_IN="/yourdirectory/monefy_csv/"
