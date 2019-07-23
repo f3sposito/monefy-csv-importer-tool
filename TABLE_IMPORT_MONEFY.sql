d393d2b5aa2f3ae4b6232e5842c925175efdae59
Francesco Esposito <33671357+frab1t@users.noreply.github.com>
2019-07-23 22:44:47 +0200

    edit user ref

--- TABLE_IMPORT_MONEFY.sql
+++ TABLE_IMPORT_MONEFY.sql
@@ -2,7 +2,7 @@
 * CREATE TABLE
 * filename: CREATE_STG_IMPORT_MONEFY.sql
 *
-* author:	@f3sposito (Francesco Esposito)
+* author:	@frsposito (Francesco Esposito)
 *************************************************/
 CREATE TABLE TABLE_IMPORT_MONEFY(
 	DATA_OP DATE,
